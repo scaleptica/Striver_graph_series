6b60c766a72183a2e1f0d6654ca0127e3bb76dfb
Kartik Mathur <60697152+scaleptica@users.noreply.github.com>
2022-08-29 23:51:35 +0530

    Update Interview_GraphLecture2.cpp

--- Interview_GraphLecture2.cpp
+++ Interview_GraphLecture2.cpp
@@ -22,11 +22,30 @@ using namespace std;
 int main() {
     int n, m;
     cin>>n>>m;
-    int adj[n+1][m+1];
-    while(m--){
-        cin>>node1>>node2;
-        adj[node1][node2] = 1;
-        adj[node2][node1] = 1;
+    vector<int> adj[n+1];
+    for(int i=0; i<m;i++){
+        int u, v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    return 0;
+}
+
+//in case of directed graph, the adjacency list will look something like=>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin>>n>>m;
+    vector<int> adj[n+1];
+    for(int i=0; i<m;i++){
+        int u, v;
+        //Given=> u--->v
+        cin>>u>>v;
+        adj[u].push_back(v);
+        //adj[v].push_back(u);
     }
     return 0;
 }
